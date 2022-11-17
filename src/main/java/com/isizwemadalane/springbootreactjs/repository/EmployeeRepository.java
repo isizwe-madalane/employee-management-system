4380246aa1c33064004e59394e23d43a0eff3ac0
isizwe-madalane <isizwe.madalane@gmail.com>
2022-11-17 09:53:07 +0200

    Revert code

--- src/main/java/com/isizwemadalane/springbootreactjs/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.isizwemadalane.springbootreactjs.repository;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.isizwemadalane.springbootreactjs.model.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-}
